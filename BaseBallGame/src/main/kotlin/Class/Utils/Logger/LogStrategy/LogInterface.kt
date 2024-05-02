6a3454158958688983e2a95b99062b7b794b7d2d
npureaun <98468118+npureaun@users.noreply.github.com>
2024-05-02 16:08:58 +0900

    리드미 수정

--- BaseBallGame/src/main/kotlin/Class/Utils/Logger/LogStrategy/LogInterface.kt
+++ BaseBallGame/src/main/kotlin/Class/Utils/Logger/LogStrategy/LogInterface.kt
@@ -2,9 +2,9 @@ package org.example.Class.Dialog.Logger.LogStrategy
 
 interface LogInterface<T> {
     val log: MutableList<T>
-    fun input(data:T)
-    fun output(n: Int):T
-    fun isEmpty():Boolean
+    fun input(data: T)
+    fun output(n: Int): T
+    fun isEmpty(): Boolean
     fun indices(): IntRange
     fun readAll()
     fun clear()
